11404e2e97b5a950a0ae446a42139fb1971d95e8
timofey1198 <timostar98@gmail.com>
2017-01-15 01:38:22 +0300

    создан класс партии

---
+++ SgfReader/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.IO;
+
+namespace SgfReader
+{
+    class Game
+    {
+        /// <param name="path">Путь к открываемому SGF файлу</param>
+        public Game(string path)
+        {
+            SGF = new FileStream(path, FileMode.Open);
+        }
+
+        // Номер текущего хода
+        [DefaultValue(0)]
+        public int Move { get; }
+
+        // Текущая позиция на доске
+        public int[,] Position { get; }
+
+
+        // Поток файла партии
+        private FileStream SGF;
+    }
+}
