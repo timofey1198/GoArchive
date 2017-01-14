091262f460187c5534b0eb0c9f32263167b50171
timofey1198 <timostar98@gmail.com>
2017-01-15 01:39:08 +0300

    новая версия

---
+++ SgfReader/Chart.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace SgfReader
+{
+    public class Chart : Control
+    {
+        [DefaultValue(20)]
+        public int Indent { get; set; }
+
+        [DefaultValue(20)]
+        public int Step { get; set; }
+
+        public Chart()
+        {
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+            Step = 20;
+            Indent = 20;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var rect = ClientRectangle;
+            rect.Inflate(-Indent, -Indent);
+            var step = Step;
+
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            //рисуем сетку
+            using (var font = new Font(Font.FontFamily, 8f))
+            using (var pen = new Pen(Color.FromArgb(50, Color.Navy), 1))
+            {
+                int i = 0;
+
+                for (float x = rect.Left; x <= rect.Right; x += step)
+                {
+                    e.Graphics.DrawLine(pen, x, rect.Bottom, x, rect.Top);
+                    e.Graphics.DrawString(i.ToString(), font, Brushes.Navy, x - 5, rect.Bottom + 5);
+                    i++;
+                }
+
+                i = 0;
+                for (float y = rect.Bottom; y >= rect.Top; y -= step)
+                {
+                    e.Graphics.DrawLine(pen, rect.Left, y, rect.Right + 5, y);
+                    e.Graphics.DrawString(i.ToString(), font, Brushes.Navy, rect.Left - 15, y - 5);
+                    i++;
+                }
+            }
+
+            //рисуем оси
+            using (Pen pen = new Pen(Color.Navy, 1))
+            {
+                pen.EndCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+                e.Graphics.DrawLine(pen, rect.Left, rect.Bottom, rect.Left, rect.Top - 10);
+                e.Graphics.DrawLine(pen, rect.Left, rect.Bottom, rect.Right + 7, rect.Bottom);
+            }
+        }
+    }
+}
---
+++ SgfReader/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace SgfReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            int size = Settings1.Default.BoardSize;
+            this.points = new Punct[size, size];
+            this.SuspendLayout();
+            // 
+            // points
+            //
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    points[i, j] = new Punct(i, j);
+                    points[i, j].Click += new EventHandler(points[i, j].Punct_Click);
+                }
+            }
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(size*40, size*35);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    this.Controls.Add(points[i, j]);
+                }
+            }
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+        private Punct[,] points;
+    }
+}
+
